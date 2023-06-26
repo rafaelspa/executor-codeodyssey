b3cdd40137e733c5344dffde2b353054ce47e0b1
Lucas Gabriel <lucasogjc@gmail.com>
2023-06-26 19:23:36 -0300

    add image listing

--- main.py
+++ main.py
@@ -5,10 +5,16 @@ docker_client = docker.from_env()
 print('Is server responsive:')
 print(docker_client.ping())
 
+images = docker_client.images.list(all=True)
+print('\nImage list:')
+for image in images:
+    print('ID: ', image.id)
+    print(f'Tags: ${image.tags}\n')
+
 containers = docker_client.containers.list(all=True)
-print('\nContainer list:')
+print('Container list:')
 for container in containers:
     print('ID: ', container.id)
     print('Name: ', container.name)
     print('Image: ', container.image)
-    print('Status: ', container.status, '\n')
+    print(f'Status: ${container.status}\n')
